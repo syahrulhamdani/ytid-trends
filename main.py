cee40e01090987505e225c1b40750ddf7704d535
Syahrul Hamdani <syahrulhamdani.ds@gmail.com>
2021-02-07 00:43:22 +0700

    feat: save trending video data

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 """Worker to extract Indonesia Trending YouTube Video Statistics."""
+import os
 import logging
+from datetime import datetime
 from time import time
 
 import pandas as pd
@@ -9,6 +11,19 @@ from ytid.logger import setup_logging
 
 
 _LOGGER = logging.getLogger(__name__)
+
+
+def save_to_csv(data, filename="trending.csv"):
+    """Save data as csv in the given datadir."""
+    if not os.path.isdir(filename.split("/")[-2]):
+        os.makedirs(filename.split("/")[-2])
+
+    if not os.path.exists(filename):
+        data.to_csv(filename, index=False)
+        return
+
+    data.to_csv(filename, index=False, header=False, mode="a")
+    return
 
 
 def main():
@@ -24,10 +39,18 @@ def main():
                   (end - start))
 
     df_videos = pd.DataFrame([
-        video.to_dict()
+        video.to_dict(trending_time=datetime.now())
         for video in videos
     ])
     _LOGGER.info("Got total %d trending videos", df_videos.shape[0])
+
+    filename = os.path.join(config.DATADIR, "trending.csv")
+    save_to_csv(df_videos, filename)
+    df_saved = pd.read_csv(filename)
+    _LOGGER.info("Done saving %d trending videos (%s). Total videos: %d",
+                 df_videos.shape[0],
+                 os.path.join(config.DATADIR, "trending.csv"),
+                 df_saved.shape[0])
 
 
 if __name__ == "__main__":
--- ytid/config.py
+++ ytid/config.py
@@ -5,3 +5,4 @@ LOG_LEVEL = os.getenv("LOG_LEVEL", "DEBUG")
 
 API_KEY = os.getenv("API_KEY", "")
 URL = os.getenv("URL", "")
+DATADIR = os.getenv("DATADIR", "data")
