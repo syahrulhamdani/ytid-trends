3aedb9aa36fc92e2143b0ca41eafbd53a8828042
Syahrul Bahar Hamdani <syahrulhamdani.ds@gmail.com>
2021-04-03 19:54:52 +0700

    feat: automate last dates retrieval (#7)
    * feat: automate last dates retrieval
    * fix: raise ReadError when filename doesn't exist

---
+++ last_retrieved_trending.py
@@ -0,0 +1,34 @@
+"""Script to check last retrieval trendings in trending.csv"""
+import os
+import logging
+from datetime import datetime
+
+import pandas as pd
+
+from ytid import config as c
+from ytid.exceptions import ReadError
+from ytid.logger import setup_logging
+
+
+_LOGGER = logging.getLogger(__name__)
+
+
+def main():
+    filename = os.path.join(c.DATADIR, "trending.csv")
+    if not os.path.exists(filename):
+        raise ReadError("File not found: %s" % filename)
+
+    df = pd.read_csv(filename, parse_dates=[-1])
+    _LOGGER.info("Loaded data: %s", filename)
+
+    last_5_dates = df.trending_time.dt.date.unique()[-5:]
+    last_5_dates = [
+        date.strftime("%Y%m%d")
+        for date in last_5_dates
+    ]
+    _LOGGER.info("Last 5 retrieval dates: %s", ", ".join(last_5_dates))
+
+
+if __name__ == "__main__":
+    setup_logging()
+    main()
